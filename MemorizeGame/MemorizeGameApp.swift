ce0b95aec3312e3bcedf58c65ca91c0081781cc9
Dmitry <dmitry.korchagin.s@gmail.com>
2023-11-27 01:05:03 +0300

    Initial Commit

---
+++ MemorizeGame/MemorizeGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeGameApp.swift
+//  MemorizeGame
+//
+//  Created by Дмитрий Корчагин on 11/27/23.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
